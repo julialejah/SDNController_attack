fd04cbcf46415ebadf3ab71905c9380eb8db2ef0
Ubuntu <juliana@MyDockerVM.kgp2arlj3voexigrw5zkhjvuwg.bx.internal.cloudapp.net>
2023-10-04 21:26:23 +0000

    start

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
+pip3 install pandas
+pip3 install scapy
 
 #Build images from dockerfile
 sudo docker build --tag=scapy -f Dockerfiles/scapy/Dockerfile  Dockerfiles/scapy
 sudo docker build --tag=odl -f Dockerfiles/odl/Dockerfile  Dockerfiles/odl
 
 #Create directory for the dockers in file "red2.py"
-sudo mkdir /home/juli/shvol
+ruta=$(pwd)
+#sudo mkdir /home/juli/shvol
 
 #run the odl docker
 sudo docker run -t -d -p 6633:6633 -p 6653:6653 -p 8181:8181 -p 8101:8101 -p 2222:22 --name odl --privileged odl 
