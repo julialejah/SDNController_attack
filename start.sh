9b62a316480940825046885e10b41206a0449721
julialejah <50000959+julialejah@users.noreply.github.com>
2023-11-08 09:18:44 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,3 +11,9 @@ ruta=$(pwd)
 
 #run the odl docker
 sudo docker run -t -d -p 6633:6633 -p 6653:6653 -p 8181:8181 -p 8101:8101 -p 2222:22 --name odl --privileged odl 
+
+##install containernet as Nested Docker deployment
+docker pull containernet/containernet
+docker run --name containernet -it --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock containernet/containernet /bin/bash
+
+
